b99d1a79cdb7b985b47e3059964d900ee4b681aa
Victor Farias <jose.oliveira@compasso.com.br>
2020-11-22 11:29:36 -0300

    adicionado criação, obter um ou muitos leilões e outras otimizações

--- slascore/auction-service/src/handlers/createAuction.js
+++ slascore/auction-service/src/handlers/createAuction.js
@@ -1,10 +1,12 @@
 import { v4 as uuid } from 'uuid'
 import AWS from 'aws-sdk'
+import createError from 'http-errors'
+import commonMiddleware from '../../lib/commonMiddleware'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body)
+  const { title } = event.body
   const now = new Date()
 
   const auction = {
@@ -14,17 +16,22 @@ async function createAuction(event, context) {
     createdAt: now.toISOString()
   }
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise()
-  
+  try {
+    
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise()
+
+  } catch (error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
+  }  
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
   };
 }
 
-export const handler = createAuction;
-
+export const handler = commonMiddleware(createAuction)
 
